61ee6ca2ed3241ddaaf906030c34ae313d91db8f
Maxime <mbinet42@gmail.com>
2022-05-23 16:06:06 +0800

    Fix purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   corePlugins: {
     container: false,
   },
