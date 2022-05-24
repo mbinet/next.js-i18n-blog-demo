8351a32168748ca37110061e6056588a8f67a855
Maxime <mbinet42@gmail.com>
2022-05-24 14:44:05 +0800

    Edit exit-preview

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,6 +1,14 @@
-export default async (_, res) => {
+export default async (req, res) => {
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData()
+
+
+  if (req.query.slug) {
+    // Redirect to the path from the fetched post
+    res.redirect(req.query.slug)
+  } else {
+    res.end('Preview mode disabled');
+  }
 
   // Redirect the user back to the index page.
   res.writeHead(307, { Location: '/' })
