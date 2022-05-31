1b6661250fd00a8ccc4f0d08689080d41e828518
Maxime <mbinet42@gmail.com>
2022-05-31 17:15:58 +0800

    Add crisp chat

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Footer from '../components/footer'
+import Script from 'next/script'
 
 export default function Layout({ preview, children }) {
   return (
@@ -7,6 +8,22 @@ export default function Layout({ preview, children }) {
         { preview && 'hey preview'}
         <main>{children}</main>
       </div>
+      <Script
+        id="crisp-widget"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.$crisp=[];
+            window.CRISP_WEBSITE_ID="a05e6b3e-0fa2-4a76-838a-527f235bfe95";
+            (function(){
+              const d = document;
+              const s = d.createElement("script");
+              s.src = "https://client.crisp.chat/l.js";
+              s.async = 1;
+              d.getElementsByTagName("head")[0].appendChild(s);
+            })();`,
+        }}
+      />
       <Footer />
     </>
   )
